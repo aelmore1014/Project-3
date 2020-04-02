bdec1d284a52ccad0744ecc8c1f5b1bf7f01ad18
Amanda Surber <surberaleigh@gmail.com>
2020-04-02 15:35:07 -0400

    adding navbar to product and cart pages

--- storeapp/src/pages/products/index.jsx
+++ storeapp/src/pages/products/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../../components/navbar";
 
 export default function ProductsPage() {
   return (
     <div>
+      <Navbar />
       <h1>Products</h1>
     </div>
   );
--- storeapp/src/pages/view-cart/index.jsx
+++ storeapp/src/pages/view-cart/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CartProvider from "../../components/cart/context";
+import Navbar from "../../components/navbar";
 
 export default function ViewCartPage() {
   return (
     <div>
       <CartProvider>
+      <Navbar />
       <h1>View Cart</h1>
       </CartProvider>
     </div>
