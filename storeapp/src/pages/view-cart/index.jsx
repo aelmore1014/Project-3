d2eb02244f3999a9ee201fee408ffeadf75e3d6c
Amanda Surber <surberaleigh@gmail.com>
2020-04-02 13:31:24 -0400

    added cart provider import for the View Cart page. Hoping it'll work once we can save cart to db

--- storeapp/src/pages/view-cart/index.jsx
+++ storeapp/src/pages/view-cart/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import CartProvider from "../../components/cart/context";
 
 export default function ViewCartPage() {
   return (
     <div>
+      <CartProvider>
       <h1>View Cart</h1>
+      </CartProvider>
     </div>
   );
 }
