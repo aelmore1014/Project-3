31c48bc11191d86dc6136f548fd5cf2172657ed0
Alex Elmore <alexelmore1014@gmail.com>
2020-04-03 18:30:06 -0400

    added background image for first page

--- storeapp/src/pages/mainpage/index.jsx
+++ storeapp/src/pages/mainpage/index.jsx
@@ -3,18 +3,23 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import Layout from "../../components/layout";
-
 import Store from "../../components/store";
+
 
 export default function MainPage() {
   return (
-    <div style={{ backgroundImage: '' }}>
+    <div style={{backgroundColor:"black"}}>
     <Layout title="Welcome to Appquire">
-      {/* <Store /> */}
-      <div>
+      {/* <Store /> */} 
+      <div style={{backgroundImage: "url(https://cdn.stocksnap.io/img-thumbs/960w/XLCKX6HJKD.jpg)", 
+      width:"60rem", 
+      height:"40rem"
+      }}>
           {/* <div><h1>About Appquire</h1></div> */}
           <br/>
-          <p>Welcome to Appquire, “The best place on the web to Aquire an App”! This app was designed to provide a platform  for all of the software developers out there who are trying to sell apps that they have created.  We came up with this idea because we are developers ourselves and wanted to work on something that could really be useful and meaningful to us and future aspiring devs.  Our team of developers are currently attending The Coding Boot Camp at UNC Charlotte and strive to give back to the tech community.  We hope you enjoy! -Appquire Dev Team</p>
+          <div>
+          <h1 style={{color:"white",textAlign:"center", fontSize:"30px"}} >Welcome to Appquire, “The best place on the web to Aquire an App”! This app was designed to provide a platform  for all of the software developers out there who are trying to sell apps that they have created.  We came up with this idea because we are developers ourselves and wanted to work on something that could really be useful and meaningful to us and future aspiring devs.  Our team of developers are currently attending The Coding Boot Camp at UNC Charlotte and strive to give back to the tech community.  We hope you enjoy! -Appquire Dev Team</h1>
+          </div>
       </div>
     </Layout>
     </div>
