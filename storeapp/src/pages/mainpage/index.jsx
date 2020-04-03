4b5a7c3520106b6ab71e21fad800c0872500d13f
Matthew Starr <buylowsellhigh@me.com>
2020-04-03 16:34:45 -0400

    fixed header About page

--- storeapp/src/pages/mainpage/index.jsx
+++ storeapp/src/pages/mainpage/index.jsx
@@ -9,10 +9,10 @@ import Store from "../../components/store";
 export default function MainPage() {
   return (
     <div>
-    <Layout title="">
+    <Layout title="Welcome to Appquire">
       {/* <Store /> */}
       <div>
-          <div><h1>About Appquire</h1></div>
+          {/* <div><h1>About Appquire</h1></div> */}
           <br/>
           <p>Welcome to Appquire, “The best place on the web to Aquire an App”! This app was designed to provide a platform  for all of the software developers out there who are trying to sell apps that they have created.  We came up with this idea because we are developers ourselves and wanted to work on something that could really be useful and meaningful to us and future aspiring devs.  Our team of developers are currently attending The Coding Boot Camp at UNC Charlotte and strive to give back to the tech community.  We hope you enjoy! -Appquire Dev Team</p>
       </div>
