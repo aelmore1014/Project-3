3b8b425bf28e345d72377b29ea46de4d970a1a52
Brittany <britloy91@gmail.com>
2020-04-11 14:21:31 -0400

    server issuesss

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ app.use(cookieSession({ secret: "hello" }));
 app.use(cors());
 app.use(express.static("public"));
 // app.use('/', indexRouter)
-app.use(express.static(path.join(__dirname, “../storeapp/build”))); // Serve static assets if in production
+// app.use(express.static(path.join(__dirname, “../storeapp/build”))); // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-    // app.use(express.static("client/build"));
-    // Set static folder
+    app.use(express.static(path.join(__dirname, “../storeapp/build”))); // Serve static assets if in production
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + “../storeapp/build/index.html”));
     });
